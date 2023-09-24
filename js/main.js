9a3bf585796c6420c120efd3d6754ff73b9b9766
BehruzRehimli <bahruzkr@code.edu.az>
2023-09-24 07:41:57 +0400

    featSlider

--- js/main.js
+++ js/main.js
@@ -4,5 +4,22 @@ fetch('https://localhost:7076/api/Sliders', {
       'Content-Type': 'application/json'
     },
   })
-    .then(response => {
-        console.log(response);})
+    .then(response => response.json()).then(data=>{
+        data.forEach(element => {
+            console.log(element);
+            var coru=document.querySelector(".carousel-inner")
+            
+            coru.innerHTML+=`
+
+          <div class="carousel-item ${element.id==3?"active":null}">
+            <img src="https://localhost:7076/image/slider/${element.İmageName}" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>${element.title}</h5>
+              <p>${element.title2}</p>
+            </div>
+          </div>
+
+
+            `
+        });
+    })
